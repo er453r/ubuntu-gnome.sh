807f55a6a928ca4e0ac0de4ae70543a708d3f8f0
Marcin Kotz <marcin@kotz.pl>
2024-05-01 22:59:26 +0200

    fix: fix for adwaita cursors

--- ubuntu-gnome.sh
+++ ubuntu-gnome.sh
@@ -11,7 +11,7 @@ apt-get autoremove -y
 apt-get install -y gnome-session gnome-backgrounds gnome-tweaks chrome-gnome-shell plymouth-theme-ubuntu-gnome-logo adwaita-* -y
 
 # update default cursors and login screen
-update-alternatives --set x-cursor-theme /etc/X11/cursors/core.theme
+update-alternatives --set x-cursor-theme /usr/share/icons/Adwaita/cursor.theme
 update-alternatives --set gdm-theme.gresource /usr/share/gnome-shell/gnome-shell-theme.gresource
 
 # remove ubuntu logo from login screen
